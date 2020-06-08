d9eaff6319363248219c3606dea9c9b8a6087451
Toby <tobyshorin@gmail.com>
2020-06-08 12:24:32 -0400

    website upload

---
+++ main.js
@@ -0,0 +1,10 @@
+document.ready(function(){
+	accordion = document.getElementById("extension");
+	// videos = document.querySelector("video");
+	accordion.addEventListener("click", => e {
+		console.log("clicked!")
+		// for(v in videos; v < videos.length; v++) {
+		// 	v.play();
+		// }
+	});
+});
