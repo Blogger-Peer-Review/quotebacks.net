53a172fe665efdea78faed5755f290c818fdd20f
Toby <tobyshorin@gmail.com>
2020-07-16 18:14:26 -0400

    dark mode

--- configurator/config.js
+++ configurator/config.js
@@ -7,3 +7,18 @@ $("#copy-embed").click(function(){
 	}, 1000);	
 });
 
+
+
+let light = $("#theme-light");
+let dark = $("#theme-dark");
+
+dark.click(function(event){
+	event.preventDefault();
+	$(".display-panel").css("background-color", "#1F1F20");
+	$(".codeblock").css({"background-color": "#606367", "border": "1px solid #606367", "color":"lightgray"});
+})
+light.click(function(event){
+	event.preventDefault();
+	$(".display-panel").css("background-color", "#fafafa");
+	$(".codeblock").css({"background-color": "#f2f2f2", "border": "1px solid #C2DFE3", "color":"gray"});
+})
